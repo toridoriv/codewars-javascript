7ada4f018a28bf7e297649ff6407c7aa764c7447
Victoria Rodriguez <vrodriguezfe@icloud.com>
2023-12-17 14:34:42 -0300

    chore: Init project

---
+++ prettier.config.js
@@ -0,0 +1,23 @@
+/** @type {PrettierConfig & Partial<PJPOptions>} */
+export default {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  printWidth: 80,
+  proseWrap: "always",
+  quoteProps: "consistent",
+  useTabs: false,
+  endOfLine: "lf",
+  bracketSameLine: true,
+  plugins: ["@homer0/prettier-plugin-jsdoc"],
+  jsdocEnsureDescriptionsAreSentences: true,
+  jsdocPrintWidth: 100,
+  jsdocTagsOrder: ["example", "template", "param", "returns", "typedef"],
+  jsdocFormatExamples: false,
+  jsdocUseInlineCommentForASingleTagBlock: true,
+};
+
+/** @typedef {import("prettier").Config} PrettierConfig */
+
+/** @typedef {import("@homer0/prettier-plugin-jsdoc/src/types.js").PJPOptions} PJPOptions */
